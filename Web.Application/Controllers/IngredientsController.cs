6ec3398d11b51362287d72786943cf69bd38bcfb
NiazbekovKuralbek <n.k.a.k.28241916@gmail.com>
2023-05-04 13:06:19 +0600

    remake_View

--- Web.Application/Controllers/IngredientsController.cs
+++ Web.Application/Controllers/IngredientsController.cs
@@ -11,38 +11,52 @@ namespace Web.Application.Controllers
 {
     public class IngredientsController : Controller
     {
+        // Здесь объявляем переменные, которые будут использоваться внутри класса
         private string? _query;
         private static int? _product;
 
+        // Экшен, который вызывается при запросе /Ingredients/Index
+        // Если указан параметр product, то будут отображены только ингредиенты для соответствующего продукта
+        // Если параметр не указан, то будет выбран первый продукт из списка и отображены все ингредиенты для него
         public IActionResult Index(int? product)
         {
             _product = product ?? ProductViewModel
                 .GetProducts()
                 .FirstOrDefault()!.Id;
 
+            // Получаем список продуктов и передаем их в выпадающий список в представлении
             ViewData["Product"] = new SelectList(ProductViewModel.GetProducts(), "Id", "Name");
+
+            // Получаем список ингредиентов для выбранного продукта и передаем их в представление
             return View(IngredientViewModel.GetIngredients(_product));
         }
 
-        /// get
+        // Экшен, который вызывается при запросе /Ingredients/Create (GET)
+        // Он открывает страницу с формой для создания нового ингредиента
         public IActionResult Create()
         {
+            // Сохраняем выбранный продукт для передачи в представление
             ViewData["_product"] = _product;
 
+            // Получаем список материалов и передаем их в выпадающий список в представлении
             ViewData["Material"] = new SelectList(MaterialViewModel.GetMaterials(), "Id", "Name");
+
             return View();
         }
 
+        // Экшен, который вызывается при отправке формы на странице создания нового ингредиента (POST)
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Ingredient ingredient)
         {
+            // Сохраняем выбранный продукт для передачи в представление
             ViewData["_product"] = _product;
+
             if (ModelState.IsValid)
             {
                 try
                 {
-                    // Если записи еще нет, то добавляем ее
+                    // Если записи еще нет, то добавляем ее в базу данных
                     _query = "usp_Ingredient_Insert";
                     using (SqlCommand sqlCommand = new SqlCommand(_query, DataBaseContext.Connection))
                     {
@@ -61,6 +75,7 @@ namespace Web.Application.Controllers
                 }
                 catch (Exception ex)
                 {
+                    // Если запись уже существует, то передаем сообщение об ошибке в представление
                     ViewData["Alredy"] = "Данная сырьё уже присутствует в ингридиенте";
                     ViewData["Product"] = new SelectList(ProductViewModel.GetProducts(), "Id", "Name", ingredient.Product);
                     ViewData["Material"] = new SelectList(MaterialViewModel.GetMaterials(), "Id", "Name", ingredient.Material); return View(ingredient);
